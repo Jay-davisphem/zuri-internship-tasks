95a235458e4bbd24425cbae0da260ddacae20d96
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-06-21 18:35:18 +0100

    submit task

---
+++ tasks/hyperlinked_web/script.js
@@ -0,0 +1,4 @@
+const form = document.querySelector("form");
+
+// focus on the the first form element
+form.firstElementChild.focus();
---
+++ tasks/simple_js/printDetails.js
@@ -0,0 +1,13 @@
+// Sorry, I used object and template literals
+const student = {
+  name: "David Oluwafemi",
+  height: 69.1,
+  country: "Nigeria",
+};
+
+console.log(
+  `My details:
+  \tname: ${student.name}
+  \theight: ${student.height} inches
+  \tcountry: ${student.country}`
+);
